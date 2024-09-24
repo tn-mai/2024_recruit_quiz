d7ab57289add3274957c88e72afe321689a0c434
tanaka mai <rabitfiver@gmail.com>
2024-09-24 20:49:57 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "[リクルート試験対策クイズ]\n";
+  cout << "13x(-5)の答えは？\n";
+
+  int answer;
+  cin >> answer;
+  if (answer == 13*-5) {
+    cout << "正解！\n";
+  } else {
+    cout << "間違い！\n";
+  }
+}
