6530aad57ef89f50876c781eb6ded065dba608b1
tanaka mai <rabitfiver@gmail.com>
2024-09-24 21:20:06 +0900

    問題数を増やす。

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Question
+{
+  string q; // 問題文
+  int a;    // 答え
+};
 
 int main()
 {
-  cout << "[リクルート試験対策クイズ]\n";
-  cout << "13x(-5)の答えは？\n";
+  Question questions[] = {
+    { "13×(-5)の答えは？\n", 13 * -5 },
+    { "(-21)÷(-3)の答えは？\n", -21 / -3 },
+    { "7-(4+2)÷2の答えは？\n", 7 - (4 + 2) / 2 },
+  };
 
-  int answer;
-  cin >> answer;
-  if (answer == 13*-5) {
-    cout << "正解！\n";
-  } else {
-    cout << "間違い！\n";
-  }
+  cout << "[リクルート試験対策クイズ]\n";
+
+  for (const auto& e : questions) {
+    cout << e.q;
+    int answer;
+    cin >> answer;
+    if (answer == e.a) {
+      cout << "正解！\n";
+    } else {
+      cout << "間違い！ 正解は" << e.a << "\n";
+    }
+  } // for questions
 }
