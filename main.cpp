74b49f8a2f58c4227c731240ffbb9d18d15e52e9
tanaka mai <rabitfiver@gmail.com>
2024-09-25 12:06:15 +0900

    問題の数字をランダムに選ぶ。

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <random>
 using namespace std;
 
 struct Question
@@ -10,11 +11,30 @@ struct Question
 
 int main()
 {
-  Question questions[] = {
-    { "13×(-5)の答えは？\n", 13 * -5 },
-    { "(-21)÷(-3)の答えは？\n", -21 / -3 },
-    { "7-(4+2)÷2の答えは？\n", 7 - (4 + 2) / 2 },
-  };
+  Question questions[3];
+
+  random_device rd;
+  mt19937 rand(rd());
+
+  // 掛け算
+  int x = uniform_int_distribution<>(1, 30)(rand);
+  int y = uniform_int_distribution<>(1, 20)(rand);
+  questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは？\n";
+  questions[0].a = x * y;
+
+  // 割り算
+  x = uniform_int_distribution<>(1, 30)(rand);
+  y = uniform_int_distribution<>(1, 20)(rand);
+  questions[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは？\n";
+  questions[1].a = x;
+
+  // 複雑な式
+  x = uniform_int_distribution<>(1, 100)(rand);
+  y = uniform_int_distribution<>(1, 10)(rand);
+  int z = uniform_int_distribution<>(1, 10)(rand);
+  int w = uniform_int_distribution<>(1, 10)(rand);
+  questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？\n";
+  questions[2].a = x - (y + z);
 
   cout << "[リクルート試験対策クイズ]\n";
 
